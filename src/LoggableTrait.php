649f98384d3034aa6744b1df437d129283741810
Rob Marton <rob.marton@mcgarrybowen.com>
2016-11-23 17:30:35 -0500

    Added log types to loggable

--- src/LoggableTrait.php
+++ src/LoggableTrait.php
@@ -6,16 +6,22 @@ use Monolog\Handler\RotatingFileHandler;
 
 trait LoggableTrait
 {
-    public function getLog()
+    public function getLog($type = 'import')
     {
         if (! $this->repositoryPath) {
             throw new \Exception('No repositoryPath set for LoggableTrait::getLog');
         }
 
-        $this->log = new Logger('memory');
-        $this->log->pushHandler(new RotatingFileHandler($this->repositoryPath . '/memory.log', Logger::WARNING));
+        if (! isset($this->log)) {
+            $this->log = [];
+        }
 
-        return $this->log;
+        if (! array_key_exists($type, $this->log)) {
+            $this->log[$type] = new Logger($type);
+            $this->log[$type]->pushHandler(new RotatingFileHandler($this->repositoryPath . '/' . $type . '.log', Logger::WARNING));
+        }
+
+        return $this->log[$type];
     }
 }
 
